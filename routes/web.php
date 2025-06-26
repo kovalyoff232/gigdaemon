4e75b16acf7397ecc85a4bb271e4d73ac065e147
kovalyoff232 <36049828+kovalyoff232@users.noreply.github.com>
2025-06-26 14:18:14 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan; // Добавь эту строчку
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +25,9 @@ Route::get('/invoices', [App\Http\Controllers\HomeController::class, 'invoices']
 Route::get('/invoices/{invoice}', [App\Http\Controllers\HomeController::class, 'showInvoice'])->name('invoices.show');
 Route::get('/invoices/{invoice}/download', [\App\Http\Controllers\Api\V1\InvoiceController::class, 'downloadPDF'])->name('invoices.download');
 
+// --- НАШ СЕКРЕТНЫЙ РЫЧАГ ---
+// Замени 'KJH234G5H6J3K4L' на любую свою абракадабру
+Route::get('/system/clear-cache/KJH234G5H6J3K4L', function() {
+    Artisan::call('cache:clear');
+    return "Application cache cleared successfully.";
+});
